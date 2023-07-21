de65d07f20570e0ff5024f52e346441013ca75aa
Alex Sh <shalex.work@gmail.com>
2023-07-21 22:17:39 +0300

    Fix the RegisterInterface

--- source/RegisterMapManager/RegisterInterface.h
+++ source/RegisterMapManager/RegisterInterface.h
@@ -5,8 +5,7 @@
 
 class RegisterInterface {
 public:
-    RegisterInterface() = default;
-    ~RegisterInterface() = default;
+    virtual ~RegisterInterface() = default;
     virtual uint32_t get(uint32_t address) = 0;
     virtual uint8_t set(uint32_t address, uint32_t value) = 0;
 };
