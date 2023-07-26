b68aa377f8383b531493d6d3615f161a123215f8
Alex Sh <shalex.work@gmail.com>
2023-07-26 14:12:46 +0300

    Add multithreading safety guards (#19)

--- source/RegisterMapManager/RegistersMapManager.h
+++ source/RegisterMapManager/RegistersMapManager.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <utility>
+#include <mutex>
 #include "RegisterInterface.h"
 #include "Registers.h"
 
@@ -13,18 +14,19 @@ public:
     }
     ~RegistersMapManager() = default;
 
-    [[nodiscard]] uint32_t getValue(REG reg) const;
-    [[nodiscard]] uint8_t setValue(REG reg, uint32_t value) const;
-    [[nodiscard]] uint8_t resetValue(REG reg) const;
-    [[nodiscard]] uint8_t clearValue(REG reg) const;
-    [[nodiscard]] uint8_t setBit(REG reg, uint8_t bit_index) const;
-    [[nodiscard]] uint8_t clearBit(REG reg, uint8_t bit_index) const;
-    [[nodiscard]] uint8_t getNibble(REG reg, uint8_t nibble_index) const;
-    [[nodiscard]] uint8_t setNibble(REG reg, uint8_t nibble_index, uint8_t nibble_value) const;
-    [[nodiscard]] uint8_t resetAll() const;
-    [[nodiscard]] uint8_t clearAll() const;
+    [[nodiscard]] uint32_t getValue(REG reg);
+    [[nodiscard]] uint8_t setValue(REG reg, uint32_t value);
+    [[nodiscard]] uint8_t resetValue(REG reg);
+    [[nodiscard]] uint8_t clearValue(REG reg);
+    [[nodiscard]] uint8_t setBit(REG reg, uint8_t bit_index);
+    [[nodiscard]] uint8_t clearBit(REG reg, uint8_t bit_index);
+    [[nodiscard]] uint8_t getNibble(REG reg, uint8_t nibble_index);
+    [[nodiscard]] uint8_t setNibble(REG reg, uint8_t nibble_index, uint8_t nibble_value);
+    [[nodiscard]] uint8_t resetAll();
+    [[nodiscard]] uint8_t clearAll();
 private:
     std::shared_ptr<RegisterInterface> register_interface_;
+    std::mutex mtx_;
 };
 
 #endif //REGMAP_MANAGER_REGISTERSMAPMANAGER_H
--- tests/RegisterMapManagerTest.cpp
+++ tests/RegisterMapManagerTest.cpp
@@ -2,6 +2,7 @@
 #include "gmock/gmock.h"
 /* Add your project include files here */
 #include "../source/RegisterMapManager/RegistersMapManager.h"
+#include <thread>
 
 // TEST PLAN
 // + Get 32bit register value via the register HW interface
@@ -17,7 +18,7 @@
 // + Set wrong nibble value > 0xF
 // + Reset all registers to default
 // + Clear all registers
-// - Test for multithreading support
+// + Test for multithreading support
 
 class RegisterMock : public RegisterInterface {
 public:
@@ -29,7 +30,7 @@ class RegisterMapManagerTest : public testing::Test {
 public:
     RegisterMapManagerTest() :
             register_interface(std::make_shared<RegisterMock>()),
-            register_map(std::make_shared<RegistersMapManager>(register_interface)){}
+            register_map(std::make_shared<RegistersMapManager>(register_interface)) {}
     std::shared_ptr<RegisterMock> register_interface;
     std::shared_ptr<RegistersMapManager> register_map;
 };
@@ -38,28 +39,28 @@ TEST_F(RegisterMapManagerTest, GetRegisterValue) {
     EXPECT_CALL(*register_interface, get(testing::_))
             .WillOnce(testing::Return(0xFFFF'FFFF));
 
-	EXPECT_EQ(register_map->getValue(REG::VIDEO), 0xFFFF'FFFF);
+    EXPECT_EQ(register_map->getValue(REG::VIDEO), 0xFFFF'FFFF);
 }
 
 TEST_F(RegisterMapManagerTest, SetRegisterValue) {
     EXPECT_CALL(*register_interface, set(testing::_, 0xFFFF'FFFF))
             .WillOnce(testing::Return(0));
 
-	EXPECT_EQ(register_map->setValue(REG::VIDEO, 0xFFFF'FFFF), 0);
+    EXPECT_EQ(register_map->setValue(REG::VIDEO, 0xFFFF'FFFF), 0);
 }
 
 TEST_F(RegisterMapManagerTest, ResetRegisterToDefault) {
     EXPECT_CALL(*register_interface, set(testing::_, testing::_))
             .WillOnce(testing::Return(0));
 
-	EXPECT_EQ(register_map->resetValue(REG::VIDEO), 0);
+    EXPECT_EQ(register_map->resetValue(REG::VIDEO), 0);
 }
 
 TEST_F(RegisterMapManagerTest, ClearRegister) {
     EXPECT_CALL(*register_interface, set(testing::_, 0))
             .WillOnce(testing::Return(0));
 
-	EXPECT_EQ(register_map->clearValue(REG::VIDEO), 0);
+    EXPECT_EQ(register_map->clearValue(REG::VIDEO), 0);
 }
 
 TEST_F(RegisterMapManagerTest, SetBit) {
@@ -68,7 +69,7 @@ TEST_F(RegisterMapManagerTest, SetBit) {
     EXPECT_CALL(*register_interface, set(testing::_, 0x0000'0001))
             .WillOnce(testing::Return(0));
 
-	EXPECT_EQ(register_map->setBit(REG::VIDEO, 0), 0);
+    EXPECT_EQ(register_map->setBit(REG::VIDEO, 0), 0);
 }
 
 TEST_F(RegisterMapManagerTest, ClearBit) {
@@ -77,12 +78,12 @@ TEST_F(RegisterMapManagerTest, ClearBit) {
     EXPECT_CALL(*register_interface, set(testing::_, 0xFFFF'FFFE))
             .WillOnce(testing::Return(0));
 
-	EXPECT_EQ(register_map->clearBit(REG::VIDEO, 0), 0);
+    EXPECT_EQ(register_map->clearBit(REG::VIDEO, 0), 0);
 }
 
 TEST_F(RegisterMapManagerTest, GetOrSetBitLargerThan31) {
     EXPECT_CALL(*register_interface, get(testing::_)).Times(0);
-    EXPECT_CALL(*register_interface, set(testing::_,testing::_)).Times(0);
+    EXPECT_CALL(*register_interface, set(testing::_, testing::_)).Times(0);
 
     EXPECT_EQ(register_map->setBit(REG::VIDEO, 32), 1);
     EXPECT_EQ(register_map->clearBit(REG::VIDEO, 32), 1);
@@ -106,7 +107,7 @@ TEST_F(RegisterMapManagerTest, SetNibble) {
 
 TEST_F(RegisterMapManagerTest, GetSetWrongNibbleIndex) {
     EXPECT_CALL(*register_interface, get(testing::_)).Times(0);
-    EXPECT_CALL(*register_interface, set(testing::_,testing::_)).Times(0);
+    EXPECT_CALL(*register_interface, set(testing::_, testing::_)).Times(0);
 
     EXPECT_EQ(register_map->getNibble(REG::VIDEO, 8), 1);
     EXPECT_EQ(register_map->setNibble(REG::VIDEO, 8, 0xf), 1);
@@ -114,7 +115,7 @@ TEST_F(RegisterMapManagerTest, GetSetWrongNibbleIndex) {
 
 TEST_F(RegisterMapManagerTest, SetWrongNibbleValue) {
     EXPECT_CALL(*register_interface, get(testing::_)).Times(0);
-    EXPECT_CALL(*register_interface, set(testing::_,testing::_)).Times(0);
+    EXPECT_CALL(*register_interface, set(testing::_, testing::_)).Times(0);
 
     EXPECT_EQ(register_map->setNibble(REG::VIDEO, 0, 0xff), 1);
 }
@@ -135,20 +136,58 @@ TEST_F(RegisterMapManagerTest, ClearAllRegisters) {
 
 TEST_F(RegisterMapManagerTest, ResetAllToDefaultFails) {
     EXPECT_CALL(*register_interface, set(testing::_, testing::_))
-    .WillRepeatedly(testing::Return(0));
+            .WillRepeatedly(testing::Return(0));
 
     EXPECT_CALL(*register_interface, set(testing::_, testing::_))
-    .WillOnce(testing::Return(1));
+            .WillOnce(testing::Return(1));
 
-EXPECT_EQ(register_map->resetAll(), 1);
+    EXPECT_EQ(register_map->resetAll(), 1);
 }
 
 TEST_F(RegisterMapManagerTest, ClearAllRegistersFails) {
     EXPECT_CALL(*register_interface, set(testing::_, testing::_))
-    .WillRepeatedly(testing::Return(0));
+            .WillRepeatedly(testing::Return(0));
 
     EXPECT_CALL(*register_interface, set(testing::_, testing::_))
-    .WillOnce(testing::Return(1));
+            .WillOnce(testing::Return(1));
 
-EXPECT_EQ(register_map->clearAll(), 1);
+    EXPECT_EQ(register_map->clearAll(), 1);
+}
+
+TEST_F(RegisterMapManagerTest, SetRegisterValueThreadSafety) {
+    auto inside_interface_set_func = std::make_shared<std::atomic<bool>>(false);
+
+    ON_CALL(*register_interface, set(testing::_, testing::_))
+            .WillByDefault([inside_interface_set_func](int address, uint32_t value) mutable {
+                if (*inside_interface_set_func) {
+                    return 1;
+                }
+                *inside_interface_set_func = true;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                *inside_interface_set_func = false;
+                return 0;
+            });
+
+    std::atomic<bool> error_flag(false);
+
+    std::thread thread1([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            if (register_map->setValue(REG::VIDEO, 0xFFFF'FFFF)) {
+                error_flag.store(true);
+            }
+        }
+    });
+
+    std::thread thread2([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            if (register_map->setValue(REG::VIDEO, 0x0000'0000)) {
+                error_flag.store(true);
+            }
+        }
+    });
+
+    thread1.join();
+    thread2.join();
+
+    EXPECT_EQ(error_flag.load(), false);
 }
